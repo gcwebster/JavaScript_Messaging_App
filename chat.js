1447f12ae168a7879469d94464d30adbb83447cc
George Webster <george.webster@nationwide.co.uk>
2020-07-06 19:56:31 +0100

    can't access any of the users functions

--- chat.js
+++ chat.js
@@ -1,20 +1,46 @@
-var person = (
+var NBS = NBS || {};
+
+NBS.init = {
+
+  function() {
+
+    NBS.User1 = new NBS.person('user1');
+    NBS.User2 = new NBS.person('user2');
+
+    function sendMessageUser1() {
+      console.log('called  function send message user 1')
+      NBS.User1.addMessage('a');
+    }
+    function sendMessageUser2() {
+      console.log('called  function send message user 2')
+      NBS.User2.addMessage('b');
+    }
+
+    return {
+      sendMessageUser1: sendMessageUser1,
+      sendMessageUser2: sendMessageUser2
+    }
+  }
+}
+
+NBS.person = (
   function (className) {
-    var className = className; // Can't be accessed outside of scope
+    var _className = className; // Can't be accessed outside of scope
+
     function addMessage(message) {
+      console.log('reached add message with parameter ' + message);
       var html = `<div class='messageBubble'><p>${message}</p></div>`;
       var newMessage = document.createElement('div');
-      newMessage.innerHTML = html; newMessage.setAttribute("class", className);
+      newMessage.innerHTML = html; newMessage.setAttribute("class", _className);
       document.getElementById('messagingBody').appendChild(newMessage);
-      console.log('reached add message with parameter ' + message);
     }
 
     function getClassName() {
-      return className;
+      return _className;
     }
 
     function setClassName(newClassName) {
-      className = newClassName;
+      _className = newClassName;
     }
 
     return {
