0a5f291742ce23c4beb68edeae4430b23d6e3560
George Webster <george.webster@nationwide.co.uk>
2020-07-10 09:16:06 +0100

    working stable build

--- chat.js
+++ chat.js
@@ -1,34 +1,29 @@
 var NBS = NBS || {};
 
-NBS.init = {
+NBS.init = (
+  function () {
+    NBS.User1 = new NBS.person('user1', 'sender1');
+    NBS.User2 = new NBS.person('user2', 'sender2');
 
-  function() {
 
-    NBS.User1 = new NBS.person('user1');
-    NBS.User2 = new NBS.person('user2');
-
-    function sendMessageUser1() {
-      console.log('called  function send message user 1')
-      NBS.User1.addMessage('a');
-    }
-    function sendMessageUser2() {
-      console.log('called  function send message user 2')
-      NBS.User2.addMessage('b');
+    // Make a message object and  initalise it for the bottom two.
+    document.getElementById('sender1').onclick = function () {
+      var messageText = document.getElementById("messageInput").value;
+      NBS.User1.addMessage(messageText);
     }
 
-    return {
-      sendMessageUser1: sendMessageUser1,
-      sendMessageUser2: sendMessageUser2
+    document.getElementById('sender2').onclick = function () {
+      var messageText = document.getElementById("messageInput").value;
+      NBS.User2.addMessage(messageText);
     }
   }
-}
+)
 
 NBS.person = (
   function (className) {
     var _className = className; // Can't be accessed outside of scope
 
     function addMessage(message) {
-      console.log('reached add message with parameter ' + message);
       var html = `<div class='messageBubble'><p>${message}</p></div>`;
       var newMessage = document.createElement('div');
       newMessage.innerHTML = html; newMessage.setAttribute("class", _className);
