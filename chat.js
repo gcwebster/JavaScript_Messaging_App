0e9e5b794dfef000cd4b42d5fbed48ba1cb31c1e
George Webster <george.webster@nationwide.co.uk>
2020-07-22 08:56:17 +0100

    introducing xhtml

--- chat.js
+++ chat.js
@@ -8,10 +8,18 @@ NBS.init = (
 
     // Make a message object and  initalise it for the bottom two.
     document.getElementById('sender1').onclick = function () {
-      var messageText = document.getElementById("messageInput").value;
-      NBS.User1.addMessage(messageText);
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          NBS.User1.addMessage(this.responseText); // We can't parse through this object
+        }
+      };
+      xhttp.open("GET", "https://jsonplaceholder.typicode.com/comments/1", true);
+      xhttp.send();
     }
 
+
+    // Homework - replace above function for user 1 with using fetch instead
     document.getElementById('sender2').onclick = function () {
       var messageText = document.getElementById("messageInput").value;
       NBS.User2.addMessage(messageText);
