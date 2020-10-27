1be5f0bd1c0b464413c4c846e3f9aaa40dbd2afd
George Webster <george.webster@nationwide.co.uk>
2020-10-27 16:26:55 +0000

    created server file

---
+++ server.js
@@ -0,0 +1,14 @@
+// this will need to be split out to its own VS code file later.
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+
+    wss.clients.forEach((client) => {
+      client.send(message);
+    });
+  });
+});
